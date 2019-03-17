0c7800894b8e5dc783b65ec60ef6281182170fb0
Saman <samanmunikar@gmail.com>
2019-03-17 08:09:41 +0545

    Basic Life in ShoppingCart

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.modal';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+recipes: Recipe[] = [
+  new Recipe('A test Recipe', 'This is a simply a test', 'https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco/https://storage.googleapis.com/gen-atmedia/3/2019/01/b983c6ba185a60b0899484be6fac15f59c05e75a.jpeg'),
+  new Recipe('A test Recipe', 'This is a simply a test', 'https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco/https://storage.googleapis.com/gen-atmedia/3/2019/01/b983c6ba185a60b0899484be6fac15f59c05e75a.jpeg')
+  
+];
 
   constructor() { }
 
